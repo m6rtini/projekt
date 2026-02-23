4e6816b812c41d9f808731e4fdd25cb5434ed059
m6rtini <148310527+m6rtini@users.noreply.github.com>
2026-02-23 12:09:29 +0200

    User.cs

---
+++ User.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+public class User
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Email { get; set; }
+
+    [Required]
+    public string PasswordHash { get; set; }
+
+    public string Role { get; set; } = "User";
+
+    public ICollection<Skill> Skills { get; set; }
+}
